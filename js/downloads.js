1f80c7783461b5098824da9ab3eda42653c345b0
Rojers227 <68253406+Rojers227@users.noreply.github.com>
2021-05-08 16:58:50 +0300

    Update downloads.js

--- js/downloads.js
+++ js/downloads.js
@@ -1,28 +1,32 @@
 
 var innerContent = document.getElementsByClassName("ebaniy-obema")[0]
-
-
 let repos = [
     {
         id: 1,
-        title: "Подсчёт онлайна из логов",
-        desc: "Помощник в подсчёте онлайна администрации, лидеров и остальных игроков. \n Для нормальной работы скрипта - отключите антивирус.",
+        title: "Сборка SAMP",
+        desc: "Сборка самп, видео в котором всё подробно расписано <a href=\"https://vk.com/rojers228\" style=\"color: rgb(255, 0, 0); text-decoration: none; font-weight: bold;\">ссылка</a>",
         url: "/scripts/online.rar",
     },
     {
         id: 2,
+        title: "Подсчёт онлайна из логов",
+        desc: "Помощник в подсчёте онлайна администавыварации, лидеров и остальных игроков. \n Для нормальной работы скрипта - отключите антивирус.",
+        url: "/scripts/online.rar",
+    },
+    {
+        id: 3,
         title: "Moonloader 0.26",
         desc: "Вносит возможность загрузки Lua-скриптов в игру, имеет всю функциональность опкодов игры, библиотеки CLEO, плагина SAMPFUNCS и добавляет свой набор новых функций для разработки.",
         url: "/scripts/moonloader.exe",
     },
     {
-        id: 3,
+        id: 4,
         title: "CLEO 4.0",
         desc: "CLEO расширяет возможности скриптинга и позволяет использовать тысячи уникальных модов , которые изменяют и дополняют процесс игры.",
         url: "/scripts/CLEO.ZIP",
     },
     {
-        id: 4,
+        id: 5,
         title: "SAMPFUNCS v5.4.1",
         desc: "SAMPFUNCS это дополнение к библиотеке CLEO 4, глобально расширяющее возможности скриптеров. ",
         url: "/scripts/sampfunks for arizona.ZIP",
@@ -40,16 +44,5 @@ if(innerContent) {
     <a class="download" href="${reposObj.url}" download>Скачать</a>
 </div>`
         innerContent.innerHTML += reposStr
-    }
-}
-
-if(innerContent) {
-    for (var i = 0; i < news.length; i++) {
-        var newsObj = news[i]
-        var newsStr = `<div class="news-element" id="news-${newsObj.id}">
-    <p class="title">${newsObj.title}</p>
-    ${newsObj.desc.replace(/\n/g,"<br>")}
-</div>`
-        innerContent.innerHTML += newsStr
     }
 }
